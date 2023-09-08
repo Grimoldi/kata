276088ceffed2b1a8666f07891d972284f8a31c0
Grimoldi Eugenio <e.grimoldi@blueit.it>
2023-09-08 10:50:51 +0200

    First approach, move the string constant to a module file, fixed the test.

--- gilded-rose/src/gilded_rose.py
+++ gilded-rose/src/gilded_rose.py
@@ -1,33 +1,34 @@
 # -*- coding: utf-8 -*-
+import src.models as m
+
 
 class GildedRose(object):
-
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name != m.AGED_BRIAR and item.name != m.BACKSTAGE_PASS:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != m.SULFURAS:
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == m.BACKSTAGE_PASS:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != m.SULFURAS:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != m.AGED_BRIAR:
+                    if item.name != m.BACKSTAGE_PASS:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != m.SULFURAS:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
@@ -37,7 +38,7 @@ class GildedRose(object):
 
 
 class Item:
-    def __init__(self, name, sell_in, quality):
+    def __init__(self, name: str, sell_in: int, quality: int):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
---
+++ gilded-rose/src/models.py
@@ -0,0 +1,4 @@
+AGED_BRIAR = "Aged Brie"
+BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+CONJURED_BREAD = "Conjured Bread"
+SULFURAS = "Sulfuras, Hand of Ragnaros"
