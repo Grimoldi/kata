42f8823a52f305d0c83b7259282a05e75a31b071
Grimoldi Eugenio <e.grimoldi@blueit.it>
2023-09-08 11:13:33 +0200

    Wrote tests for each case.

--- gilded-rose/tests/test_backstage_pass.py
+++ gilded-rose/tests/test_backstage_pass.py
@@ -4,6 +4,8 @@ import unittest
 import src.models as m
 from src.gilded_rose import GildedRose, Item
 
+MAX_QUALITY = 50
+
 
 class GildedRoseTest(unittest.TestCase):
     def test_backstage_quality_default_increase(self):
@@ -15,6 +17,16 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(quality + 1, items[0].quality)
 
+    def test_backstage_quality_default_increase_le_fifty(self):
+        # "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+        # The Quality of an item is never more than 50
+        sell_in = 20
+        quality = MAX_QUALITY
+        items = [Item(m.BACKSTAGE_PASS, sell_in, quality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(MAX_QUALITY, items[0].quality)
+
     def test_backstage_quality_double_increase(self):
         # Quality increases by 2 when there are 10 days or less
         sell_in = 10
@@ -24,6 +36,16 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(quality + 2, items[0].quality)
 
+    def test_backstage_quality_double_increase_le_fifty(self):
+        # Quality increases by 2 when there are 10 days or less
+        # The Quality of an item is never more than 50
+        sell_in = 10
+        quality = MAX_QUALITY - 1
+        items = [Item(m.BACKSTAGE_PASS, sell_in, quality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(MAX_QUALITY, items[0].quality)
+
     def test_backstage_quality_triple_increase(self):
         # by 3 when there are 5 days or less
         sell_in = 5
@@ -32,6 +54,16 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(quality + 3, items[0].quality)
+
+    def test_backstage_quality_triple_increase_le_fifty(self):
+        # by 3 when there are 5 days or less
+        # The Quality of an item is never more than 50
+        sell_in = 5
+        quality = MAX_QUALITY - 2
+        items = [Item(m.BACKSTAGE_PASS, sell_in, quality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(MAX_QUALITY, items[0].quality)
 
     def test_backstage_quality_drop(self):
         # Quality drops to 0 after the concert
